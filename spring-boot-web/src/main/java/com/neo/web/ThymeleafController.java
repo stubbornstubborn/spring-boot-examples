96b88e7e6263ad36f96eaeac2d941c344385b139
stubborn <33212030+hfut-nmw@users.noreply.github.com>
2021-09-12 17:52:43 +0800

    统一Sprint boot 版本

--- spring-boot-web/src/main/java/com/neo/web/ThymeleafController.java
+++ spring-boot-web/src/main/java/com/neo/web/ThymeleafController.java
@@ -10,13 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ThymeleafController {
-	
+
     @RequestMapping("/hi")
 	public String hello(Locale locale, Model model) {
 		model.addAttribute("greeting", "Hello!");
-
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);        
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
 		model.addAttribute("currentTime", formattedDate);
 
